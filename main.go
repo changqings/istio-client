5c12694ef5c91dbf3fcbabdc9f6b16c3e324f0a3
shenchangqing <shenchangqing@126.com>
2022-05-09 01:20:43 +0800

    Neet to do...

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"fmt"
+	"istio-client/client"
+	"istio-client/istio"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var cs = client.GetIstioClient()
 
 func main() {
+
+	test()
+
+	// os exit
+	fmt.Printf("\nThis is for dev, exit main.go\n")
+	os.Exit(0)
+	// go fiber run belew
 	app := fiber.New(fiber.Config{
-		ReadBufferSize:  8092,
-		WriteBufferSize: 8092,
+		ReadBufferSize:  8192,
+		WriteBufferSize: 8192,
 		AppName:         "istio-client",
 	})
 
@@ -15,5 +32,25 @@ func main() {
 	})
 
 	app.Listen("localhost:18086")
+
+}
+
+func test() {
+
+	var vs = &istio.Vs{
+		Name:      "nginx-vs",
+		Namespace: "shencq",
+	}
+
+	v := vs.GetVs(cs)
+	vCopy := v.DeepCopy()
+	vCopy.ObjectMeta.Annotations = nil
+	vCopy.ObjectMeta.ManagedFields = nil
+	vCopy.ObjectMeta.SelfLink = ""
+	vCopy.ObjectMeta.UID = ""
+	vCopy.ObjectMeta.ResourceVersion = ""
+
+	vb, _ := json.Marshal(vCopy)
+	fmt.Println(string(vb))
 
 }
